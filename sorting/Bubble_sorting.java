f4298c265102ce63568b207edb2c1f49930ff714
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-07-06 07:31:38 +0000

    #

--- sorting/Bubble_sorting.java
+++ sorting/Bubble_sorting.java
@@ -1,6 +1,7 @@
  public class Bubble_sorting{
     public static void Bubblesort(int arr[]){
         int n = arr.length;
+        boolean flag= false;
         for(int i= 0; i<n; i++){
             for(int j=0; j<n-i-1; j++){
                 // last element are already sorted so using n-i-1
@@ -8,7 +9,12 @@
                     int temp=arr[j];
                     arr[j]= arr[j+1];
                     arr[j+1]= temp;
+                    flag= true;
+                    // if array is unsorted then swap will done that leads to flag true  
                 }
+            }
+            if(flag==false){ // this block of code is for optimize the best case scenario which is showing that if the array is already sorted then no need to traverse the array just return
+                return;
             }
         }
     }
