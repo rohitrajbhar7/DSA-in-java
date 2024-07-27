4b27f663dc60751473a896a1ce83d56bb6437a12
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-07-27 11:08:29 +0000

    #

---
+++ OOPs/Inheritence/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+       advCalc r= new advCalc();
+       System.out.println(r.add(7,5));
+       System.out.println(r.sub(7,5));
+       System.out.println(r.mul(7,5));
+       System.out.println(r.div(20.0f,5.0f));
+    }
+}
---
+++ OOPs/Inheritence/advCalc.java
@@ -0,0 +1,8 @@
+public class advCalc extends calc{
+    public int mul(int a, int b){
+        return a*b;
+    } 
+    public float div(float a, float b){
+        return a/b;
+    } 
+}
