9958737ee79a001a031290eede1d7a854874fd0c
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-07-27 11:31:27 +0000

    #

--- OOPs/Inheritence/Main.java
+++ OOPs/Inheritence/Main.java
@@ -1,9 +1,24 @@
 public class Main{
     public static void main(String[] args){
-       advCalc r= new advCalc();
-       System.out.println(r.add(7,5));
-       System.out.println(r.sub(7,5));
-       System.out.println(r.mul(7,5));
-       System.out.println(r.div(20.0f,5.0f));
+    //    advCalc r= new advCalc();
+    //    System.out.println(r.add(7,5));
+    //    System.out.println(r.sub(7,5));
+    //    System.out.println(r.mul(7,5));
+    //    System.out.println(r.div(20.0f,5.0f));
+
+
+    multiLevel r= new multiLevel();
+    System.out.println(r.add(7,5));
+    System.out.println(r.sub(7,5));
+    System.out.println(r.mul(7,5));
+    System.out.println(r.div(20.0f,5.0f));
+    System.out.println(r.power(7.0,5.0));
     }
 }
+// java does not support multiple inheritence because of ambiguity
+
+//problem of ambiguity---->
+// suppose we have a class A and it has a method of show()
+// and we have a class B and it has a method of show()
+// we have a class C who is child class of both and if child class want to access the show method it does not accessed 
+// because the compiler compiler confused which class of show method u r calling
---
+++ OOPs/Inheritence/multiLevel.java
@@ -0,0 +1,5 @@
+public class multiLevel extends advCalc{
+    public double power(double a, double b){
+        return Math.pow(a, b);
+    }
+}
